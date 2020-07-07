3cd5011c8d5e2ed904def6a65badb8475ebdde5a
JaehyunKim94 <didwjd169@gmail.com>
2020-07-07 22:54:21 +0900

    Fix minitab fontsize & replace

--- src/components/portfolio/minitab/Minitab.vue
+++ src/components/portfolio/minitab/Minitab.vue
@@ -5,26 +5,25 @@
       <v-tab>Mini Profile</v-tab>
       <v-tab>Blog</v-tab>
       <v-tab-item>
-        <v-card class="tabcard">
-          <v-card-text color="transparent">Hi</v-card-text>
-        </v-card>
+        <TabText />
       </v-tab-item>
       <v-tab-item>
-        <v-card>
-          <v-card-text color="transparent">Hello</v-card-text>
-        </v-card>
+        <TabText />
       </v-tab-item>
       <v-tab-item>
-        <v-card>
-          <v-card-text>How</v-card-text>
-        </v-card>
+        <TabText />
       </v-tab-item>
     </v-tabs>
   </v-container>
 </template>
 
 <script>
-export default {};
+import TabText from './tabText/TabText.vue'
+export default {
+  components: {
+    TabText
+  }
+};
 </script>
 
 <style src="./Minitab.css" scoped></style>
---
+++ src/components/portfolio/minitab/tabText/TabText.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="tabText">
+      <br>
+      <h1>hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./TabText.css" scoped></style>
