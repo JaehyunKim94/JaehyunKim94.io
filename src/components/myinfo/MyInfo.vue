51d1aac575e3ca25065ef6ce9a9f54fe86aa7d5a
JaehyunKim94 <didwjd169@gmail.com>
2020-07-08 23:15:07 +0900

    Add info component

---
+++ src/components/myinfo/MyInfo.vue
@@ -0,0 +1,18 @@
+<template>
+<v-container class="page">
+    <v-row class="page2">
+        <v-col cols="6">
+            <h1>hello</h1>
+        </v-col>
+        <v-col cols="6"></v-col>
+    </v-row>
+</v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./MyInfo.css" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <Portfolio />
+    <MyInfo />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Portfolio from '@/components/portfolio/Portfolio.vue'
-
+import MyInfo from '@/components/myinfo/MyInfo.vue'
 export default {
   name: 'Home',
   components: {
-    Portfolio
+    Portfolio,
+    MyInfo
   }
 }
 </script>
